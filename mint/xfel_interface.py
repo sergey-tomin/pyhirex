2a56650875536344124831f4d4e58317ecd51528
sergey-tomin <tomin.sergey@gmail.com>
2019-10-03 14:26:34 +0200

    added gauss spectrum for testing

--- mint/xfel_interface.py
+++ mint/xfel_interface.py
@@ -354,7 +354,8 @@ class TestMachineInterface(XFELMachineInterface):
         """
         #if "QUAD" in device_name:
         #    return 0
-        return np.random.rand(1280) #self.data
+        spectrum  = 10*np.exp(-np.linspace(-10, 10, num=1280)**2/((2*2))) + 10*np.exp(-np.linspace(-8, 12, num=1280)**2/((2*0.25))) + np.random.rand(1280)
+        return  spectrum
 
     def set_value(self, device_name, val):
         """
